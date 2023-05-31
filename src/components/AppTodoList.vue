b521d0c580cf8bc7a53b6b32a5ea0589bfb3f436
maxsvst <fioklin2015@mail.ru>
2023-05-31 14:02:27 +0300

    Декомпозциция на компоненты

---
+++ src/components/AppTodoList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="todo-list">
+    <AppTodoItem />
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppTodoItem from "./AppTodoItem.vue";
+
+export default defineComponent({
+  components: {
+    AppTodoItem,
+  },
+});
+</script>
